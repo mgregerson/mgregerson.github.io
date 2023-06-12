c3be83ae4fd1d3da0b37936de2f3d8cafa13de7d
mgregerson <mgregerson4321@gmail.com>
2023-06-12 19:19:52 -0400

    Removed pseudocode on Title

--- src/Title.js
+++ src/Title.js
@@ -47,25 +47,3 @@ function Title({ showPage }) {
 }
 
 export default Title;
-
-// <div className="Title card mx-auto my-auto pt-5">
-//   <div className="row g-0">
-//     <div className="col-md-4">
-//       <img
-//         src="https://i.imgur.com/x84wDrJ.png"
-//         className="card-img-left img-fluid"
-//         alt="It's Me!"
-//         width="400"
-//         height="400"
-//       />
-//     </div>
-//     <div className="col-md-8">
-//       <div className="card-body">
-//         <p className="Title card-text">
-//           Hi. I'm Matt, and I'm a Software Engineer .
-//         </p>
-//         <button onClick={showPage}>Welcome</button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
